1383a390f9e7f98795edde9cf7fafca998025ea9
Ajinkya-Ban <ajinkyaban@gmail.com>
2025-05-09 15:28:35 +0530

    update and delete api code deployed

--- controllers/userController.js
+++ controllers/userController.js
@@ -28,4 +28,77 @@ const getAllData = (req, res) => {
   });
 };
 
-module.exports = { addNewUser, getAllData };
+const updateUser = (req, res) => {
+  const { uname, age, email, mobile, uadd } = req.body;
+  const update_user_sql =
+    "update users set uname = ?, age=?, email=?,mobile=?,uadd=? where id =?";
+  db.query(
+    update_user_sql,
+    [uname, age, email, mobile, uadd, req.params.id],
+    (err, result) => {
+      if (err) {
+        return res.status(500).send({
+          success: false,
+          error: err,
+        });
+      } else {
+        res.status(200).send({
+          success: true,
+          message: "user updated",
+        });
+      }
+    }
+  );
+};
+
+const deleteUser = (req, res) => {
+  const sql_delete = "delete from users where id = ?";
+  db.query(sql_delete, [req.params.id], (err, result) => {
+    if (err) {
+      return res.status(500).send({
+        success: false,
+        error: err,
+      });
+    }
+    if (result.affectedRows === 0) {
+      return res.status(404).send({
+        success: true,
+        message: "User not found",
+      });
+    } else {
+      res.status(200).send({
+        success: true,
+        message: "user deleted",
+      });
+    }
+  });
+};
+
+const getDataByID = (req, res) => {
+  const id = req.params.id;
+  search_sql = "select * from users where id = ?";
+  db.query(search_sql, [id], (err, result) => {
+    if (err) {
+      return res.status(500).send({
+        sucess: false,
+        error: err,
+      });
+    }
+    if (result.length === 0) {
+      return res.status(404).send({
+        sucess: true,
+        message: "User not found",
+      });
+    } else {
+      res.status(200).json(result);
+    }
+  });
+};
+
+module.exports = {
+  addNewUser,
+  getAllData,
+  updateUser,
+  deleteUser,
+  getDataByID,
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,18 @@
 const express = require("express");
-const { addNewUser, getAllData } = require("../controllers/userController");
+const {
+  addNewUser,
+  getAllData,
+  updateUser,
+  deleteUser,
+  getDataByID,
+} = require("../controllers/userController");
 
 const router = express.Router();
 
 router.post("/add-user", addNewUser);
 router.get("/get-data", getAllData);
+router.put("/users/:id", updateUser);
+router.delete("/users/:id", deleteUser);
+router.get("/get-data/:id", getDataByID);
 
 module.exports = router;
